b5d6fdde29f0769b2a35b5c0ba007b137dac0236
Nick Nasky <nick.nasky@gmail.com>
2017-06-12 16:10:00 -0600

    Completed 1st exercise

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-// Your JS goes here
+function checkerboard() {
+  for (var i = 0; i < 81; i++) {
+    var space = document.createElement("div");
+    document.body.appendChild(space);
+    space.style.width = "11.1%";
+    space.style.paddingBottom = "11.1%";
+    space.style.float = "left";
+    if (i % 2) {
+      space.style.backgroundColor = "red";
+    } else {
+      space.style.backgroundColor = "black";
+    }
+  }
+}
+document.body.style.margin = "0";
+checkerboard();
