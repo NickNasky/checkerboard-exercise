0cf819ec4cc50b6988135a6d1efa2819cc7ee42f
Nick Nasky <nick.nasky@gmail.com>
2017-06-13 11:47:38 -0600

    completed flashing

--- script.js
+++ script.js
@@ -2,21 +2,27 @@ function checkerboard() {
   for (var i = 0; i < 81; i++) {
     var space = document.createElement("div");
     document.body.appendChild(space);
-    document.body.style.backgroundImage = "linear-gradient(0, white, blue)";
-    if (i % 2){
-      space.style.width = "11.1%";
-      space.style.paddingBottom = "11.1%";
-      space.style.float = "left";
-      space.style.backgroundColor = "red";
-      space.style.opacity = ".5"
-    } else {
-      space.style.width = "11.1%";
-      space.style.paddingBottom = "11.1%";
-      space.style.float = "left";
-      space.style.backgroundColor = "white";
-      space.style.opacity = ".5"
-    }
+    space.style.width = "11.1%";
+    space.style.paddingBottom = "11.1%";
+    space.style.float = "left";
+  }
+}
+
+function color() {
+  var color1 = Math.floor(Math.random()*256);
+  var color2 = Math.floor(Math.random()*256);
+  var color3 = Math.floor(Math.random()*256);
+  return "rgb" + "(" + color1 + ", " + color2 + ", " + color3 + ")";
+}
+
+function flash() {
+  var space = document.getElementsByTagName("div");
+  for (var i = 0; i < 81; i++) {
+    space[i].style.backgroundColor = color();
   }
 }
 document.body.style.margin = "0";
 checkerboard();
+setInterval(function() {
+  flash()
+}, 2000);
